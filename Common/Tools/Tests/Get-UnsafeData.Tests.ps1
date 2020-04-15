044cbc06aea434d0ccc43eaf7a8a481355a14a3a
Thomas Démoulins <tdemoulins@gmail.com>
2020-04-15 21:30:07 +0200

    Improve reliability & logs

---
+++ Common/Tools/Tests/Get-UnsafeData.Tests.ps1
@@ -0,0 +1,45 @@
+﻿. "$PSScriptRoot/../../../Tests/Initialize-Tests.ps1" $MyInvocation
+
+InModuleScope "Tools" {
+    Describe "Get-UnsafeData" {
+        $FakeName  = "Name"
+        $FakeItem1 = @{ }
+        $FakeItem2 = @{ }
+        $FakeItem3 = @{ }
+
+        It "returns source with single item if no errors" {
+            # Act
+            $actualValue = { $FakeItem1 } | Get-UnsafeData $FakeName
+            # Assert
+            $actualValue | Should -BeExactly $FakeItem1
+        }
+
+        It "returns source with multiple items if no errors" {
+            # Act
+            $actualValue = { $FakeItem1, $FakeItem2 } | Get-UnsafeData $FakeName
+            # Assert
+            $actualValue | Should -HaveCount 2
+            $actualValue | Should -BeExactly $FakeItem1, $FakeItem2
+        }
+
+        It "returns multiple sources if no errors" {
+            # Act
+            $actualValue = { $FakeItem1 }, { $FakeItem2, $FakeItem3 } | Get-UnsafeData $FakeName
+            # Assert
+            $actualValue | Should -HaveCount 3
+            $actualValue | Should -BeExactly $FakeItem1, $FakeItem2, $FakeItem3
+        }
+
+        It "returns sources even if some errors" {
+            # Arrange
+            $FakeMessage = "Message"
+            Mock "Write-Verbose" -ParameterFilter { $Message -like "$FakeName*$FakeMessage" } -Verifiable
+            # Act
+            $actualValue = { $FakeItem1 }, { throw $FakeMessage }, { $FakeItem2 } | Get-UnsafeData $FakeName
+            # Assert
+            Assert-VerifiableMock
+            $actualValue | Should -HaveCount 2
+            $actualValue | Should -BeExactly $FakeItem1, $FakeItem2
+        }
+    }
+}
---
+++ Common/Tools/Tests/Set-HostBufferSize.Tests.ps1
@@ -0,0 +1,64 @@
+﻿. "$PSScriptRoot/../../../Tests/Initialize-Tests.ps1" $MyInvocation
+
+InModuleScope "Tools" {
+    Describe "Set-HostBufferSize" {
+        It "sets a new width" {
+            # Arrange
+            $FakeWidth = 1
+            $FakeHost = @{ UI = @{ RawUI = @{ BufferSize = @{} } } }
+            Mock "Get-Host" { $FakeHost } -Verifiable
+            # Act
+            Set-HostBufferSize -Width $FakeWidth
+            # Assert
+            Assert-VerifiableMock
+            $FakeHost.UI.RawUI.BufferSize.Width | Should -BeExactly $FakeWidth
+        }
+
+        It "sets a new height" {
+            # Arrange
+            $FakeHeight = 1
+            $FakeHost = @{ UI = @{ RawUI = @{ BufferSize = @{} } } }
+            Mock "Get-Host" { $FakeHost } -Verifiable
+            # Act
+            Set-HostBufferSize -Height $FakeHeight
+            # Assert
+            Assert-VerifiableMock
+            $FakeHost.UI.RawUI.BufferSize.Height | Should -BeExactly $FakeHeight
+        }
+
+        It "doesn't set a width too small" {
+            # Arrange
+            $FakeWidth = 1
+            $FakeHost = @{ UI = @{ RawUI = @{ BufferSize = @{}; WindowSize = @{ Width = $FakeWidth + 1 } } } }
+            Mock "Get-Host" { $FakeHost } -Verifiable
+            # Act
+            Set-HostBufferSize -Width $FakeWidth
+            # Assert
+            Assert-VerifiableMock
+            $FakeHost.UI.RawUI.BufferSize.Width | Should -BeNullOrEmpty
+        }
+
+        It "doesn't set a height too small" {
+            # Arrange
+            $FakeHeight = 1
+            $FakeHost = @{ UI = @{ RawUI = @{ BufferSize = @{}; WindowSize = @{ Height = $FakeHeight + 1 } } } }
+            Mock "Get-Host" { $FakeHost } -Verifiable
+            # Act
+            Set-HostBufferSize -Height $FakeHeight
+            # Assert
+            Assert-VerifiableMock
+            $FakeHost.UI.RawUI.BufferSize.Height | Should -BeNullOrEmpty
+        }
+
+        It "warns if unable to set host buffer size" {
+            # Arrange
+            $FakeMessage = "Message"
+            Mock "Get-Host" { throw $FakeMessage } -Verifiable
+            Mock "Write-Warning" -ParameterFilter { $Message -like "Unable to set host buffer size: $FakeMessage" } -Verifiable
+            # Act
+            Set-HostBufferSize
+            # Assert
+            Assert-VerifiableMock
+        }
+    }
+}
